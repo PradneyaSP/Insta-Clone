fe5744b5ca58eaa21be9a24dd5e3512d7f3e7c38
pradneya-prabhudesai-meesho <pradneya.prabhudesai@meesho.com>
2026-01-12 19:19:01 +0530

    Login Activity completed

--- app/src/main/java/com/example/myinsta/LoginActivity.kt
+++ app/src/main/java/com/example/myinsta/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.myinsta
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myinsta.databinding.ActivityLoginBinding
+import androidx.core.content.edit
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -13,6 +16,22 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        val userPrefs = getSharedPreferences("UserPrefs", MODE_PRIVATE)
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if(email == "user@example.com" && password == "123456789"){
+                userPrefs.edit { putBoolean("isLoggedIn", true) }
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Incorrect Credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
         setContentView(binding.root)
     }
 }
