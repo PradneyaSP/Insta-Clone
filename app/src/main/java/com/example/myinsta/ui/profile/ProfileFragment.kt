f8aa915309fbf47a93388ada2ca940db2b61626f
pradneya-prabhudesai-meesho <pradneya.prabhudesai@meesho.com>
2026-01-18 12:56:27 +0530

    Implemented logout functionality in ProfileFragment

--- app/src/main/java/com/example/myinsta/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/myinsta/ui/profile/ProfileFragment.kt
@@ -1,18 +1,43 @@
 package com.example.myinsta.ui.profile
 
+import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
+import androidx.core.flagging.Flags
 import androidx.fragment.app.Fragment
+import com.example.myinsta.LoginActivity
 import com.example.myinsta.R
+import com.example.myinsta.databinding.FragmentProfileBinding
 
 class ProfileFragment: Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile,container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            val userPrefs = requireContext().getSharedPreferences("UserPrefs", MODE_PRIVATE)
+            userPrefs.edit {
+                putBoolean("isLoggedIn" , false)
+            }
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
